36d46c7bff6ee53bba7d9898e7f1766a5a24f6c4
tsukabo <tsukabo0809@gmailcom>
2016-02-23 15:15:00 +0900

    テストケースの作成

---
+++ tests/Fixture/UsersFixture.php
@@ -0,0 +1,53 @@
+<?php
+namespace Reincarnation\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ *
+ */
+class UsersFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 10, 'autoIncrement' => true, 'default' => null, 'null' => false, 'comment' => 'id', 'precision' => null, 'unsigned' => null],
+        'name' => ['type' => 'text', 'length' => null, 'default' => null, 'null' => true, 'comment' => 'name', 'precision' => null],
+        'delete_flg' => ['type' => 'boolean', 'length' => null, 'default' => 0, 'null' => false, 'comment' => 'delete_flg', 'precision' => null],
+        'deleted' => ['type' => 'timestamp', 'length' => null, 'default' => null, 'null' => true, 'comment' => 'deleted', 'precision' => null],
+        'created' => ['type' => 'timestamp', 'length' => null, 'default' => null, 'null' => true, 'comment' => 'created', 'precision' => null],
+        'modified' => ['type' => 'timestamp', 'length' => null, 'default' => null, 'null' => true, 'comment' => 'modified', 'precision' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'name' => 'test',
+            'delete_flg' => false,
+            'deleted' => null,
+            'created' => 1430991176,
+            'modified' => 1430991176
+        ],
+        [
+            'name' => 'test2',
+            'deleted_flg' => true,
+            'deleted' => null,
+            'created' => 1430991176,
+            'modified' => 1430991176
+        ],
+    ];
+}
---
+++ tests/test_app/App/Model/Table/UsersTable.php
@@ -0,0 +1,60 @@
+<?php
+namespace Reincarnation\Test\App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Users Model
+ */
+class UsersTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        $this->table('users');
+        $this->displayField('name');
+        $this->primaryKey('id');
+        $this->addBehavior('Timestamp');
+
+        $this->addBehavior('Reincarnation.SoftDelete', ['boolean' => 'delete_flg', 'datetime' => 'deleted']);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->add('id', 'valid', ['rule' => 'numeric'])
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->allowEmpty('name');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        return $rules;
+    }
+}
